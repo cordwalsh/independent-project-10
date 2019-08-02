0ea215d96096414c1ddd5e6d4f81c4fb596b62ce
Cord Walsh <cordwalsh@Cords-MacBook-Air.local>
2019-08-02 11:24:35 -0700

    working on app.rb

--- app.rb
+++ app.rb
@@ -7,3 +7,18 @@ require("pg")
 also_reload('lib/**/*.rb')
 
 DB = PG.connect({:dbname => "volunteer_tracker_test"})
+
+get('/') do
+  Project.clear()
+  redirect to('/projects')
+end
+
+get ('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+get ('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
